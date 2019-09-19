d97a60e1ee7ae6c3f22187787e5798a4b6bcc58f
Najahiiii <najahiii@outlook.co.id>
2019-09-19 16:45:57 +0700

    sensors: Silence this annoying logs
    Signed-off-by: Najahiiii <najahiii@outlook.co.id>

--- drivers/sensors/sensors_class.c
+++ drivers/sensors/sensors_class.c
@@ -237,7 +237,6 @@ static ssize_t sensors_delay_store(struct device *dev,
 		return -EINVAL;
 	}
 	if (sensors_cdev->sensors_poll_delay == NULL) {
-		dev_err(dev, "Invalid sensor class delay handle\n");
 		return -EINVAL;
 	}
 	ret = sensors_cdev->sensors_poll_delay(sensors_cdev, data);
