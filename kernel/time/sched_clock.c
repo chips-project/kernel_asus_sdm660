ce70716ada9debf2308d30bf7c739375543715fe
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2019-10-02 18:44:30 +0700

    kernel: time: A lill cosmetics
    before: [12154.788680] suspend ns:   12154788664141\x09suspend cycles:     2473524620007
    
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

--- kernel/time/sched_clock.c
+++ kernel/time/sched_clock.c
@@ -294,8 +294,8 @@ static int sched_clock_suspend(void)
 
 	suspend_ns = rd->epoch_ns;
 	suspend_cycles = rd->epoch_cyc;
-	pr_info("suspend ns:%17llu	suspend cycles:%17llu\n",
-				rd->epoch_ns, rd->epoch_cyc);
+	pr_info("suspend ns:%17llu\n", rd->epoch_ns);
+	pr_info("suspend cycles:%17llu\n", rd->epoch_cyc);
 	hrtimer_cancel(&sched_clock_timer);
 	rd->read_sched_clock = suspended_sched_clock_read;
 
